3219c90fb63fdf75c338b74ae3070e2fd39e34b6
givensuman <givensuman@duck.com>
2025-05-23 21:56:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# fish-zoxide
+
+A port of [ohmyzsh/zoxide](https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/zoxide) for the Fish shell.
+
+### Installation
+
+Install with [fisher](https://github.com/jorgebucaran/fisher):
+
+```shell
+fisher install givensuman/fish-zoxide
+```
+
+### Usage
+
+Initializes zoxide with necessary functions and completions for Fish.
+
+Set the `zoxide_cmd_override` environment variables to override the default zodie command.
+
+```shell
+# replace `cd` with `zoxide`
+set -gx zoxide_cmd_override cd
+```
+
+### Requirements
+
+Just requires [zoxide](https://github.com/ajeetdsouza/zoxide)!
+
+### License
+
+[MIT](../LICENSE)
---
+++ conf.d/fish-zoxide.fish
@@ -0,0 +1,27 @@
+function _fish_zoxide_install --on-event fish-zoxide_install
+    if command -q zoxide
+
+        if set -q zoxide_cmd_override
+            set cmd zoxide init --cmd $zoxide_cmd_override fish
+        else
+            set cmd zoxide init fish
+        end
+
+        eval $cmd | source
+    else # `zoxide` command not found
+        echo "zoxide is not installed but you're"
+        echo "sourcing the fish plugin for it"
+
+        return 1
+    end
+
+end
+
+function _fish_zoxide_uninstall --on-event fish-zoxide_uninstall
+    set --erase zoxide
+end
+
+function _fish_zoxide_update --on-event fish-zoxide_update
+    _fish_zoxide_uninstall
+    _fish_zoxide_install
+end
