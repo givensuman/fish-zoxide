cb7563de67207cf66507a24836bafbd60b35fd5d
givensuman <givensuman@duck.com>
2025-10-27 21:52:58 -0400

    Pull non-install logic out

--- conf.d/fish-zoxide.fish
+++ conf.d/fish-zoxide.fish
@@ -1,20 +1,17 @@
-function _fish_zoxide_install --on-event fish-zoxide_install
-    if command -q zoxide
+if command -q zoxide
 
-        if set -q zoxide_cmd_override
-            set cmd zoxide init --cmd $zoxide_cmd_override fish
-        else
-            set cmd zoxide init fish
-        end
-
-        eval $cmd | source
-    else # `zoxide` command not found
-        echo "zoxide is not installed but you're"
-        echo "sourcing the fish plugin for it"
-
-        return 1
+    if set -q zoxide_cmd_override
+        set cmd zoxide init --cmd $zoxide_cmd_override fish
+    else
+        set cmd zoxide init fish
     end
 
+    eval $cmd | source
+else # `zoxide` command not found
+    echo "zoxide is not installed but you're"
+    echo "sourcing the fish plugin for it"
+
+    return 1
 end
 
 function _fish_zoxide_uninstall --on-event fish-zoxide_uninstall
