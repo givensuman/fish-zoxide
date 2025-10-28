60fd7eb7279fd857ff1d2e769fca85c1a1c4f167
givensuman <givensuman@duck.com>
2025-10-28 08:18:40 -0400

    Remove problematic check

--- conf.d/fish-zoxide.fish
+++ conf.d/fish-zoxide.fish
@@ -1,18 +1,10 @@
-if command -q zoxide
-
-    if set -q zoxide_cmd_override
-        set cmd zoxide init --cmd $zoxide_cmd_override fish
-    else
-        set cmd zoxide init fish
-    end
-
-    eval $cmd | source
-else # `zoxide` command not found
-    echo "zoxide is not installed but you're"
-    echo "sourcing the fish plugin for it"
-
-    return 1
+if set -q zoxide_cmd_override
+    set cmd zoxide init --cmd $zoxide_cmd_override fish
+else
+    set cmd zoxide init fish
 end
+
+eval $cmd | source
 
 function _fish_zoxide_uninstall --on-event fish-zoxide_uninstall
     set --erase zoxide_cmd_override
