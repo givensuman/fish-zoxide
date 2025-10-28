2bfe1a66bf09b4c5cf58d8ab06fa883b19013df1
givensuman <givensuman@duck.com>
2025-10-28 12:20:36 -0400

    Add back command check

--- conf.d/fish-zoxide.fish
+++ conf.d/fish-zoxide.fish
@@ -1,10 +1,20 @@
+if not command -q zoxide
+  echo "zoxide is not installed but you're"
+  echo "sourcing the fish plugin for it"
+
+  return 1
+end
+
 if set -q zoxide_cmd_override
-    set cmd zoxide init --cmd $zoxide_cmd_override fish
+    set cmd 'zoxide init --cmd $zoxide_cmd_override fish'
 else
-    set cmd zoxide init fish
+    set cmd 'zoxide init fish'
 end
 
 eval $cmd | source
+
+function _fish_zoxide_install --on-event fish-zoxide_install
+end
 
 function _fish_zoxide_uninstall --on-event fish-zoxide_uninstall
     set --erase zoxide_cmd_override
